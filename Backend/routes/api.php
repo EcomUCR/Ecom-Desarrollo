c675c165be5d628a860a47f8a0e09a644cab478b
AndresSequeira <andresxdsequeira@gmail.com>
2025-08-30 23:11:34 -0600

    Instalación de componentes de react de breeze

--- Backend/routes/api.php
+++ Backend/routes/api.php
@@ -1,31 +1,8 @@
 <?php
 
-use Illuminate\Support\Facades\Route;
-use App\Http\Controllers\UserController;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Route;
 
 Route::middleware(['auth:sanctum'])->get('/user', function (Request $request) {
     return $request->user();
 });
-
-// ---------------------
-// Public routes
-// ---------------------
-
-// Register a user (client or vendor)
-Route::post('/register', [UserController::class, 'register']);
-
-// Login
-//Route::post('/login', [UserController::class, 'login']);
-
-// ---------------------
-// Protected routes
-// ---------------------
-Route::middleware('auth:sanctum')->group(function () {
-
-    // Get logged-in user info
-    Route::get('/me', [UserController::class, 'me']);
-
-    // You could add more protected routes here, e.g. logout, update profile, etc.
-});
-// 
