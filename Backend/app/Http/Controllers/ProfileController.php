91bee086a5366b6018a869134a026a165ed7d171
JohnHito <johnjairo988@gmail.com>
2025-09-02 18:52:26 -0600

    merge

--- Backend/app/Http/Controllers/ProfileController.php
+++ Backend/app/Http/Controllers/ProfileController.php
@@ -61,34 +61,3 @@ class ProfileController extends Controller
         return Redirect::to('/');
     }
 }
-
-<?php
-
-namespace App\Http\Controllers;
-
-use Illuminate\Http\Request;
-use App\Models\User;
-
-class ProfileController extends Controller
-{
-    // GET /api/profiles/{id}
-    public function show($id)
-    {
-        $user = User::find($id);
-
-        // Depuración: Verifica los datos del usuario
-        \Log::info('Datos del usuario:', $user ? $user->toArray() : []);
-
-        if (!$user) {
-            return response()->json(['message' => 'Perfil no encontrado'], 404);
-        }
-
-        return response()->json([
-            'id' => $user->id,
-            'username' => $user->username,
-            'name' => $user->name,
-            'last_name' => $user->last_name,
-            'image' => $user->image, // Asegúrate de tener este campo en tu tabla users
-        ]);
-    }
-}
