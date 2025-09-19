8033f7bd0d1a2adee58ba3f12416829e4aeb0412
RealNavi <joseraulhmjr91220@gmail.com>
2025-09-18 18:11:21 -0600

    Animaciones

--- Ecom/src/componentsRaul/pages/LoginRegisterPageRaul.tsx
+++ Ecom/src/componentsRaul/pages/LoginRegisterPageRaul.tsx
@@ -10,17 +10,33 @@ export default function LoginRegisterPageRaul() {
     return (
         <div>
             <NavBar />
-            <section className='flex justify-center items-center'>
-                <div className='flex flex-col justify-center items-center bg-gradient-to-br from-contrast-main via-contrast-secondary to-main h-[90vh] w-[35%] gap-4'>
+            <section className='flex justify-center items-center font-quicksand '>
+                <div className='flex pl-60 items-center bg-gradient-to-br from-contrast-main via-contrast-secondary to-main h-[90vh] w-[35%] gap-4 2xl:px-20'>
                     {/*Botón de inicio de sesión */}
-                    <button
-                        className={`text-white font-semibold py-2 px-4 rounded-full transition ${view === 'login' ? 'bg-contrast-secondary' : 'bg-contrast-main text-contrast-secondary'}`}
-                        onClick={() => setView('login')}>Iniciar sesión</button>
-                    {/*Botón de registro */}
-                    <button
-                        className={`text-white font-semibold py-2 px-4 rounded-full transition ${view === 'register' ? 'bg-contrast-secondary' : 'bg-contrast-main text-contrast-secondary'}`}
-                        onClick={() => setView('register')}
-                    >Registrarse</button>
+                    <ul className='flex flex-col absolute top-70 ml-3 gap-20 2xl:px-60'>
+                        
+                            <div className={`bg-white absolute 2xl:left-50 -left-5 z-0 h-30 w-55  2xl:w-95 rounded-l-full transform transition-all duration-300 ${view === 'login' ? ' -top-6' : ' translate-y-30'} `}>
+                                <div className="-rotate-90 absolute w-10 h-10 -top-6 -right-3 bg-transparent flex items-center justify-center rounded-2xl">
+                                    <div className="absolute w-full h-full border-l-[1rem] border-b-[1rem] border-white rounded-bl-[6rem]"></div>
+                                </div>
+                                <div className="-rotate-180 absolute w-10 h-10 -bottom-6 -right-3 bg-transparent flex items-center justify-center rounded-2xl">
+                                    <div className="absolute w-full h-full border-l-[1rem] border-b-[1rem] border-white rounded-bl-[6rem]"></div>
+                                </div>
+                            </div>
+                        
+                        <li className='relative flex items-center'>
+                            <button
+                                className={`z-1 text-xl w-full font-semibold py-5 pl-5 rounded-full transition ${view === 'login' ? 'text-contrast-secondary' : ' text-white'}`}
+                                onClick={() => setView('login')}>Iniciar sesión</button>
+                            {/*Botón de registro */}
+                        </li>
+                        <li className='relative flex items-center'>
+                            <button
+                                className={`z-2 text-xl w-full font-semibold py-5 pl-5 rounded-full transition ${view === 'register' ? 'text-contrast-secondary' : ' text-white'}`}
+                                onClick={() => setView('register')}
+                            >Registrarse</button>
+                        </li>
+                    </ul>
                 </div>
                 <div className='flex flex-col items-center justify-center h-[90vh] w-[65%]'>
                     {/* Este div lo que hace es cargar el formulario de inicio de sesión o de registro */}
